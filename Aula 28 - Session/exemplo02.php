11b79137057b111334baaee7c9b0731b0f1786eb
Diego Mendes <dgo.mendes20@gmail.com>
2020-04-16 00:43:10 -0300

    Sessões

--- Aula 28 - Session/exemplo02.php
+++ Aula 28 - Session/exemplo02.php
@@ -1,6 +1,12 @@
 <?php
 require_once("config.php");
 
+//a função session_unset LIMPA a variável de sessão informada
+//Caso não seja informado a variável de sessão noa função, serão 
+//limpas todas as variáveis de sessão
+session_unset($_SESSION['nome']);
+
+//
 echo($_SESSION["nome"]);
 
 
