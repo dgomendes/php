87ea5d54574009ac1fb0071596446e6501cf1953
Diego Mendes <dgo.mendes20@gmail.com>
2020-04-16 00:22:44 -0300

    Constantes e Arrays Constantes

---
+++ Aula 27/exemplo01.php
@@ -0,0 +1,11 @@
+<?php
+    /*Para criar constantes, usa-se a função define("NOMEEMMAIUSCULO","valor da constante",
+    Se a constante é Case Sensitive, true ou false
+
+    Para a impressão da constante na tela, não é necessario o uso de $.
+    */
+    define("SERVIDOR","127.0.0.1",false);
+
+    echo SERVIDOR;
+
+?>
