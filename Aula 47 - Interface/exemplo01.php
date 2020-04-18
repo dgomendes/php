f9b7934fdc23d57b43ce545355893bfda8d2cf78
Diego Mendes <dgo.mendes20@gmail.com>
2020-04-18 14:26:51 -0300

    Interface

---
+++ Aula 47 - Interface/exemplo01.php
@@ -0,0 +1,31 @@
+<?php
+    interface Veiculo
+    {
+        public function acelerar($velocidade);
+        public function frenar($velocidade);
+        public function trocarMarcha($marcha);
+
+
+    }
+
+    class Civic implements Veiculo
+    {
+        public function acelerar($velocidade)
+        {
+            echo "O veículo acelerou ate $velocidade km/h";
+        }
+
+        public function frenar($velocidade)
+        {
+            echo "O veículo frenou ate a velocidade $velocidade km/h";
+        }
+
+        public function trocarMarcha($marcha)
+        {
+            echo "O veículo engatou a $marcha ° marcha.";
+        }
+    }
+    $carro = new Civic();
+    $carro->trocarMarcha(4);
+    
+?>
