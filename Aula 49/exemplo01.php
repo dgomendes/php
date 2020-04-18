8a6cca9b3c43069867442d1caea90d8ff6dcc0f1
Diego Mendes <dgo.mendes20@gmail.com>
2020-04-18 15:54:49 -0300

    Polimorfismo

---
+++ Aula 49/exemplo01.php
@@ -0,0 +1,68 @@
+<?php
+    //Polimorfismo
+    abstract class Animal
+    {
+        public function falar()
+        {
+            return "Som";
+        }
+    
+        public function mover()
+        {
+            return "Anda";
+        }
+    }
+    
+
+    class Cachorro extends Animal
+    {
+        public function falar()
+        {
+            return "Late";
+        }
+    }
+
+    class Gato extends Animal
+    {
+        public function falar()
+        {
+            return "Mia";
+        }
+    }
+
+    class Passaro extends Animal
+    {
+        public  function falar()
+        {
+            return "Canta";
+        }
+
+        public function mover()
+        {
+            return "Voa e" . parent:: mover();
+        }
+    }
+    $pluto = new Cachorro();
+
+    echo $pluto->falar() . "</br>";
+    echo $pluto->mover() . "</br>";
+
+    //
+
+    echo '===========================================';
+    echo '</br>';
+
+    
+    $garfield = new Gato();
+    echo $garfield->falar(). "</br>";
+    echo $garfield->mover(). "</br>";
+
+    echo '===========================================';
+    echo '</br>';
+    
+    
+    $ave = new Passaro();
+    echo $ave->falar(). "</br>";
+    echo $ave->mover(). "</br>";
+    
+?>
