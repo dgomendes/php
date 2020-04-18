55ec766a78748d09d42380113519ec64dd9f8627
Diego Mendes <dgo.mendes20@gmail.com>
2020-04-18 19:10:29 -0300

    Namespace

---
+++ Aula 51/index.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("config.php");
+
+    use Cliente\Cadastro;
+
+    $cad = new Cadastro();
+    $cad->setNome("Djalma Sindeaux");
+    $cad->setEmail("djalma@gmail.com");
+    $cad->setSenha("123456789");
+
+    $cad->registrarVenda();
+?>
