7435fbfd533f5d9216d60c5787edc26f83a6980a
Diego Mendes <dgo.mendes20@gmail.com>
2020-04-19 19:28:59 -0300

    Update PDO

---
+++ Aula 58/exemplo03.php
@@ -0,0 +1,11 @@
+<?php
+$conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","");
+
+$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario = :ID");
+$id = "3";
+
+$stmt->bindParam(":ID", $id);
+$stmt->execute();
+
+echo "Delete ok";
+?>
